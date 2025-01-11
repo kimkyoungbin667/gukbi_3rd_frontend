b24902904239424878280fa411d89d86d044da81
kkb <rudqls1144@naver.com>
2025-01-11 13:38:17 +0900

    style: 메인 페이지 디자인 수정

--- src/comp/components/general/Main.js
+++ src/comp/components/general/Main.js
@@ -38,14 +38,15 @@ function Main() {
 
     // 포스트잇 위치 설정 (아이템별 위치 다르게)
     const postitPositions = {
-        1: { left: "1080px", top: "500px" },
-        2: { right: "300px", top: "500px" },
-        3: { left: "600px", top: "550px" },
+        1: { left: "1080px", top: "525px" },
+        2: { right: "1400px", top: "1375px" },
+        3: { right: "800px", top: "2200px" },
     };
 
     return (
         <div className="main-container">
             <div className="wrapper">
+
                 {/* 첫 번째 아이템 */}
                 <div
                     className="item"
@@ -55,17 +56,21 @@ function Main() {
                 >
                     <div className="polaroid">
                         <img src={walking} alt="walking" />
-                        <div className="caption">행복하고 건강한 일상</div>
+                        <div className="caption">일상을 스마트하게..</div>
                     </div>
                 </div>
 
+
                 {hoveredItem === 1 && (
-                    <img
-                        src={postit}
-                        alt="postit"
-                        className="postit"
-                        style={{ ...postitPositions[1], ...animationStyle }}
-                    />
+                    <div className="postit" style={{ ...postitPositions[1], ...animationStyle }}>
+                        <img src={postit} alt="postit" className="postit-img" />
+                        <p className="postit-text">
+                            <span className="highlight-text">오늘 얼마나 걸었을까요?</span> <br />
+                            산책 기록과 칼로리 소모량 등을 <br />
+                            AI가 분석해  반려동물의 건강을 <br />
+                            스마트하게 관리해보세요!
+                        </p>
+                    </div>
                 )}
 
                 {/* 두 번째 아이템 */}
@@ -77,17 +82,21 @@ function Main() {
                 >
                     <div className="polaroid">
                         <img src={care} alt="care" />
-                        <div className="caption">소중한 하루</div>
+                        <div className="caption">소중한 일상을 기록하며..</div>
                     </div>
                 </div>
 
                 {hoveredItem === 2 && (
-                    <img
-                        src={postit}
-                        alt="postit"
-                        className="postit"
-                        style={{ ...postitPositions[2], ...animationStyle }}
-                    />
+                    <div className="postit" style={{ ...postitPositions[2], ...animationStyle }}>
+                        <img src={postit} alt="postit" className="postit-img" />
+                        <p className="postit-text">
+                            <span className="highlight-text">내 아이의 소중한 성장 이야기</span> <br />
+                            매일 달라지는 반려동물의 변화, <br />
+                            일상의 작은 순간들이 쌓여가는  <br /> 
+                            모습을 한눈에 확인하고 특별하게 <br /> 
+                            기록해보세요!
+                        </p>
+                    </div>
                 )}
 
                 {/* 세 번째 아이템 */}
@@ -104,13 +113,17 @@ function Main() {
                 </div>
 
                 {hoveredItem === 3 && (
-                    <img
-                        src={postit}
-                        alt="postit"
-                        className="postit"
-                        style={{ ...postitPositions[3], ...animationStyle }}
-                    />
+                    <div className="postit" style={{ ...postitPositions[3], ...animationStyle }}>
+                        <img src={postit} alt="postit" className="postit-img" />
+                        <p className="postit-text">
+                            <span className="highlight-text">같은 마음, 같은 이야기</span> <br />
+                            다른 반려인들과 소통하며 <br />
+                            정보를 나누고, 소중한 추억을 <br />
+                            함께 만들어보세요!
+                        </p>
+                    </div>
                 )}
+
             </div>
         </div>
     );
